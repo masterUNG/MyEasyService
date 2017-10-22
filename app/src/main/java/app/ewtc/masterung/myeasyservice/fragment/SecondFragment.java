111facd90f91f7381c8b23d7dbadfe2bc62fba26
MasterUNG <phrombutr@gmail.com>
2017-10-22 11:06:35 +0700

    Back Controller OK

--- app/src/main/java/app/ewtc/masterung/myeasyservice/fragment/SecondFragment.java
+++ app/src/main/java/app/ewtc/masterung/myeasyservice/fragment/SecondFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import app.ewtc.masterung.myeasyservice.R;
 
@@ -15,10 +16,33 @@ import app.ewtc.masterung.myeasyservice.R;
 
 public class SecondFragment extends Fragment{
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Back Controller
+        backController();
+
+
+    }   // Main Method
+
+    private void backController() {
+        Button button = getView().findViewById(R.id.btnBack);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+    }
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second, container, false);
         return view;
     }
+
 }   // Main Class
