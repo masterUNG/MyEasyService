5399ef289fafca4e7a9723d919f4623fe1d26016
MasterUNG <phrombutr@gmail.com>
2017-10-22 14:45:04 +0700

    Hambeger Icon fisish

--- app/src/main/java/app/ewtc/masterung/myeasyservice/MainActivity.java
+++ app/src/main/java/app/ewtc/masterung/myeasyservice/MainActivity.java
@@ -1,16 +1,24 @@
 package app.ewtc.masterung.myeasyservice;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import app.ewtc.masterung.myeasyservice.fragment.MainFragment;
+import app.ewtc.masterung.myeasyservice.fragment.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +34,54 @@ public class MainActivity extends AppCompatActivity {
         //Text Controller
         textController();
 
+        //Create Toolbar
+        createToolbar();
+
+
 
     }   // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    private void createToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolBarMain);
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                MainActivity.this,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+        );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
 
     private void textController() {
         TextView mainTextView = (TextView) findViewById(R.id.txtMainFragment);
@@ -38,6 +92,11 @@ public class MainActivity extends AppCompatActivity {
         mainTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFragmentMain, new MainFragment())
+                        .commit();
+
 
                 //Close Drawer
                 drawerLayout.closeDrawers();
@@ -51,6 +110,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFragmentMain, new SecondFragment())
+                        .commit();
+
                 drawerLayout.closeDrawers();
 
             }
@@ -61,6 +124,8 @@ public class MainActivity extends AppCompatActivity {
         exitTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                finish();
 
                 drawerLayout.closeDrawers();
 
